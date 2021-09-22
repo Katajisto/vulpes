6c8cb72f9253dce1e953e6ad0306ba3f1a4bd671
Katajisto <tuomas.katajisto@tuni.fi>
2021-09-22 18:41:21 +0300

    improve graph page

--- webcomponents/demo/src/App.svelte
+++ webcomponents/demo/src/App.svelte
@@ -1,14 +1,10 @@
 <script>
   import '../../lib';
-  export let name;
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-
   <div class="container">
-    <my-component></my-component>
+    <data-graph></data-graph>
   </div>
 </main>
 
--- webcomponents/lib/DataGraph.wc.svelte
+++ webcomponents/lib/DataGraph.wc.svelte
@@ -1,22 +1,88 @@
 <svelte:options tag="data-graph"/>
 
+
+
 <script>
-  import Line from "svelte-chartjs/src/Line.svelte"
+import { onMount } from "svelte";
+let canvas
+
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+);
+
+onMount(()=> {
+    let ctx = canvas.getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        datasets: [{
+            label: 'Saunan lämpötila',
+            data: [{x: 10, y: 10}, {x: 20, y: 20}, {x: 30, y: 30}, {x: 40, y: 40}, {x: 50, y: 50}],
+            backgroundColor: [
+                "#707070"
+        ],
+            borderWidth: 5,
+            borderColor: "#707070"
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});})
+
 </script>
 
-<Line 
-width={400}
-height={400}
-data={{
-    datasets: [
-        {
-            label: "Saunan lämpötila",
-            data: [
-                {x: 1, y: 20},
-                {x: 2, y: 28},
-                {x: 3, y: 17},
-                {x: 4, y: 19},
-            ]
-        }
-    ],
-}} />
+<canvas bind:this={canvas} height="70"></canvas>
