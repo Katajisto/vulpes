6e0b357edbf170a77cb48cf1d2146b83726d7e20
Tuomas Katajisto <t@ktj.st>
2023-01-14 00:15:21 +0200

    change database file location

--- models/dbConnect.go
+++ models/dbConnect.go
@@ -8,7 +8,7 @@ import (
 // Inits sqlite database for dev purposes.
 
 func connectDB() *gorm.DB {
-	db, err := gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("./database/app.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
